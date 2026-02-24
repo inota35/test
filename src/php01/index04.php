693b0abb27508355409237236311cd1c2e19aecb
inoue futa <futa.kendo3355@gmail.com>
2026-02-24 15:51:36 +0900

    コミットメッセージ

---
+++ src/php01/index04.php
@@ -0,0 +1,14 @@
+<?php
+$a=15;
+$b=3;
+$c=10;
+$d=5;
+
+$a=$b;
+$c+=$d;
+
+echo $a;
+echo "<br />";
+echo $c;
+echo "<br />";
+
---
+++ src/php01/index07.php
@@ -0,0 +1,9 @@
+<?php
+function text($a, $b)
+{
+  $add = $a + $b;
+  return $value;
+}
+
+$total = text(2, 3);
+echo $total;
